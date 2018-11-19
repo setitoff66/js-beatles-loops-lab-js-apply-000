9d42a637491b8ffde685084b8a40a46669516f6e
Sean Hendry <sean.c.hendry@gmail.com>
2018-11-19 18:35:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-var musicianString;
-var musicianInstrument=[];
+var array = [];
 for(var i=0; i<musicians.length; i++){
-musicianString= musicians[i] + ' plays ' + instruments[i];
+array.push(musicians[i] + ' plays ' + instruments[i]);
 musicianInstrument[i]=musicianString;
 }
 return musicianInstrument;
